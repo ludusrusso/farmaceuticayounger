a62014b7e9ac7a02eec7e5e3d2b727a4ffff54b6
Ludovico Russo <ludus.russo@gmail.com>
2018-08-17 19:16:27 +0200

    up preview

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,11 +1,7 @@
 import React from "react";
 import CMS from "netlify-cms";
 
-import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
-import ProductsPreview from "./cms-preview-templates/products";
-import ValuesPreview from "./cms-preview-templates/values";
-import ContactPreview from "./cms-preview-templates/contact";
 
 
 // Example of creating a custom color widget
@@ -21,9 +17,5 @@ class ColorControl extends React.Component {
 }
 
 CMS.registerPreviewStyle("/css/main.css");
-CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
-CMS.registerPreviewTemplate("products", ProductsPreview);
-CMS.registerPreviewTemplate("values", ValuesPreview);
-CMS.registerPreviewTemplate("contact", ContactPreview);
 CMS.registerWidget("color", ColorControl);
