cf18f62736bb5979ed4291267817347a599b6259
Ludovico Russo <ludus.russo@gmail.com>
2019-06-17 20:58:14 +0200

    add pages

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,24 +1,26 @@
-import React from "react";
-import CMS from "netlify-cms";
+import React from 'react';
+import CMS from 'netlify-cms';
 
-import PostPreview from "./cms-preview-templates/post";
-
+import PostPreview from './cms-preview-templates/post';
 
 // Example of creating a custom color widget
 class ColorControl extends React.Component {
   render() {
-    return <input
-        style={{height: "80px"}}
+    return (
+      <input
+        style={{ height: '80px' }}
         type="color"
         value={this.props.value}
-        onInput={(e) => this.props.onChange(e.target.value)}
-    />;
+        onInput={e => this.props.onChange(e.target.value)}
+      />
+    );
   }
 }
 
-CMS.registerPreviewStyle("/css/main.css");
-CMS.registerPreviewTemplate("post", PostPreview);
-CMS.registerPreviewTemplate("hotthisweek", PostPreview);
-CMS.registerPreviewTemplate("pharmacronimi", PostPreview);
-CMS.registerPreviewTemplate("pharmaquotes", PostPreview);
-CMS.registerWidget("color", ColorControl);
+CMS.registerPreviewStyle('/css/main.css');
+CMS.registerPreviewTemplate('post', PostPreview);
+CMS.registerPreviewTemplate('hotthisweek', PostPreview);
+CMS.registerPreviewTemplate('pharmacronimi', PostPreview);
+CMS.registerPreviewTemplate('pharmaquotes', PostPreview);
+CMS.registerPreviewTemplate('pages', PostPreview);
+CMS.registerWidget('color', ColorControl);
